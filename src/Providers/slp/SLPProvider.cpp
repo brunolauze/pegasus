d32cc6a90125b39efe5b22255d3049cef298d6ca
Bruno Lauze <brunolauze@msn.com>
2014-01-25 15:59:49 -0600

    Fixing SLP Provider

--- src/Providers/slp/SLPProvider.cpp
+++ src/Providers/slp/SLPProvider.cpp
@@ -195,7 +195,7 @@ const char protocolVersionAttribute[] = "ProtocolVersion";
 const char functionalProfilesSupportedAttribute[] =
                "FunctionalProfilesSupported";
 
-const char functionalProfileDescriptionsAttribute[] =
+const __unused char functionalProfileDescriptionsAttribute[] =
                "FunctionalProfileDescriptions";
 
 const char otherProfileDescriptionAttribute[] =
@@ -207,7 +207,7 @@ const char multipleOperationsSupportedAttribute[] =
 const char authenticationMechanismsSupportedAttribute[] =
                "AuthenticationMechanismsSupported";
 
-const char otherAuthenticationDescriptionsAttribute[] =
+const __unused char otherAuthenticationDescriptionsAttribute[] =
                "OtherAuthenticationDescription";
 
 const char authenticationMechanismDescriptionsAttribute[] =
